c7dc440a8d7ee2eb5986b595918416306c32e8e3
季悠然 <youranreus@qq.com>
2021-01-17 21:42:36 +0800

    姑且保存一下

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="header">
+    <div id="header-container" class="clear">
+      <div class="left" id="header-left">
+        <h3>团委管理系统</h3>
+      </div>
+      <div class="right" id="header-right">
+        <nav>
+          <router-link to="/">首页</router-link>
+          <router-link to="/about">关于</router-link>
+          <router-link to="/u/login">登录</router-link>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+#header{
+  height: 80px;
+  line-height: 80px;
+  background: RGBA(250,250,250,1);
+}
+#header-container{
+  width: 800px;
+  margin: 0 auto;
+}
+#header-container h3{
+  padding: 0;
+  margin: 0;
+  font-size: 1.5rem;
+}
+nav a{
+  font-size: large;
+  margin-left: 15px;
+  position: relative;
+}
+nav a::before{
+  content: '·';
+  position: absolute;
+  color: #07F;
+  font-size: 350%;
+  top: -40px;
+  left: -13px;
+  opacity: 0;
+  transition: all .3s;
+}
+.router-link-exact-active::before{
+  opacity: 1;
+}
+</style>
---
+++ src/components/User/Login.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="Login">
+  <h2>登录</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
