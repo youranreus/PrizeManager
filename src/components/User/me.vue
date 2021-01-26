962a1a4e4202d54941c271285908957af5bc8f0f
季悠然 <youranreus@qq.com>
2021-01-26 23:10:36 +0800

    完善一些页面 更新了user.me页面的侧边栏雏形

--- src/components/User/me.vue
+++ src/components/User/me.vue
@@ -1,15 +1,32 @@
 <template>
 <div>
+  <a-row>
+    <a-col :span="18" :push="6">
+      <div class="main yue">
+        <h2 style="margin: 0;">我的</h2>
 
+        <p>当是某些操作页面或者管理页面时，偏向于双栏布局，方便用户的使用</p>
+      </div>
+    </a-col>
+    <a-col :span="6" :pull="18">
+      <sidebar/>
+    </a-col>
+  </a-row>
 </div>
 </template>
 
 <script>
+import sidebar from '@/components/User/sidebar.vue'
 export default {
-  name: "me"
+  name: "me",
+  components:{
+    sidebar
+  }
 }
 </script>
 
 <style scoped>
-
+.main{
+  padding-left: 20px;
+}
 </style>
---
+++ src/components/User/sidebar.vue
@@ -0,0 +1,62 @@
+<template>
+<div id="sidebar">
+  <div class="section">
+    <li>我的奖状</li>
+    <li>我的证书</li>
+    <li>其他荣誉</li>
+  </div>
+  <div class="section">
+    <li>我的信息</li>
+    <li>我的xx</li>
+    <li>其他xx</li>
+  </div>
+  <div class="section">
+    <li>bla bla</li>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "sidebar"
+}
+</script>
+
+<style scoped>
+#sidebar{
+  padding-right: 10px;
+}
+.section{
+  background: rgba(250,250,250,1);
+  border-radius: 10px;
+  margin-bottom: 30px;
+  overflow: hidden;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.section li{
+  list-style-type: none;
+  display: block;
+  padding: 10px 15px;
+  text-align: center;
+  font-weight: bold;
+  cursor: pointer;
+  -webkit-transition: .2s ease-out all;
+  -moz-transition: .2s ease-out all;
+  -ms-transition: .2s ease-out all;
+  -o-transition: .2s ease-out all;
+  transition: .2s ease-out all;
+}
+/*.section li:first-of-type{*/
+/*  padding-top: 15px;*/
+/*}*/
+/*.section li:last-of-type{*/
+/*  padding-bottom: 15px;*/
+/*}*/
+
+.section li:hover{
+  background: black;
+  color:#fff;
+}
+
+</style>
